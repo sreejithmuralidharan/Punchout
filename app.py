02baf8f5406c6e24361174f3b65cbb0870216910
Sreejith Muralidharan <sreejith.m@hotmail.co.uk>
2024-06-21 10:41:36 +0100

    fixed secret key

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.register_blueprint(main)
 # Configure the app using environment variables
 app.config['DEBUG'] = os.getenv('FLASK_DEBUG', 'false').lower() in ['true', '1', 't']
 app.config['ENV'] = 'development' if app.config['DEBUG'] else 'production'
+app.config['SECRET_KEY'] = '8f4b8a3df1294ab6b2c5e657d8329f7fbc32e1ab7b6dc2d8c0e64f3a2e1f3c9e'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
